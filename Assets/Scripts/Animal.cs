2bd483000ae389bc57341e76626c82abf3995b23
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-06-27 20:59:45 +0300

    Added Encapsulation and main action for animals

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -4,5 +4,30 @@ using UnityEngine.UI;
 
 public abstract class Animal : MonoBehaviour
 {
-   
+    //ENCAPSULATION
+    [SerializeField]private float speed = 1f;
+    protected Vector3 startPosition;
+    protected Vector3 movePosition;
+    protected bool isMove = false;
+
+    private float startTime;
+    private float distanceLength;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public virtual void Action(Button btn)
+    {
+        btn.interactable = false;
+
+        startTime = Time.time;
+        distanceLength = Vector3.Distance(startPosition, movePosition);
+    }
 }
