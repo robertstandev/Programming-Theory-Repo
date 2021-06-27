3e3c2c4278f2d30c3bc9c671648e97ca2563aa3c
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-06-27 21:01:48 +0300

    Added POLYMORPHISM

--- Assets/Scripts/Chick.cs
+++ Assets/Scripts/Chick.cs
@@ -4,5 +4,12 @@ using UnityEngine.UI;
 //INHERITANCE
 public class Chick : Animal
 {
+    //POLYMORPHISM
+    public override void Action(Button btn)
+    {
+        movePosition = startPosition + Vector3.forward;
+        isMove = true;
 
+        base.Action(btn);
+    }
 }
