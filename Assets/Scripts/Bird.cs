0b46000190e13fa7ba9f68fe0886bb7e525b70a8
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-06-27 21:01:24 +0300

    Added POLYMORPHISM

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,5 +4,12 @@ using UnityEngine.UI;
 //INHERITANCE
 public class Bird : Animal
 {
-
+    //POLYMORPHISM
+    public override void Action(Button btn)
+    {
+        movePosition = startPosition + Vector3.left;
+        isMove = true;
+        
+        base.Action(btn);
+    }
 }
