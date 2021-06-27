70fcff2e5dd3d9f923c048da98112c6765ddc1d7
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-06-27 21:02:07 +0300

    Added POLYMORPHISM

--- Assets/Scripts/RedFox.cs
+++ Assets/Scripts/RedFox.cs
@@ -4,5 +4,12 @@ using UnityEngine.UI;
 //INHERITANCE
 public class RedFox : Animal
 {
+    //POLYMORPHISM
+    public override void Action(Button btn)
+    {
+        movePosition = startPosition + Vector3.right;
+        isMove = true;
 
+        base.Action(btn);
+    }
 }
